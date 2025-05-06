504c1924e5d018752bee194ee5886222e1fcc807
Ido Yanay <ido.s.yanay@gmail.com>
2025-05-06 10:32:37 +0300

    resources files

---
+++ empty_solution.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+### HELPER FUNCTIONS ###
+# Add here any helper functions which you think will be useful
+
+
+### Exercise Solution ###
+
+def pratical_1_runner(save_path=None):
+    pass
+
+def practical_2_runner(save_path=None):
+    pass
+
+
+if __name__ == "__main__":
+    path = None
+    pratical_1_runner(save_path=path)
+    practical_2_runner(save_path=path) 
